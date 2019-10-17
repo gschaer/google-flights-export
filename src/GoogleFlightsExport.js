c33635e01e71b2a544e74aca96050b8a0922494f
Guillaume <guillaume@2far.ch>
2019-10-17 12:35:25 +0200

    Fix vars

--- src/GoogleFlightsExport.js
+++ src/GoogleFlightsExport.js
@@ -7,45 +7,45 @@
 // ==/Bookmarklet==
 
 var copyListener = event => {
-	trip = "";
-	tripHTML = "";
+	var trip = "";
+	var tripHTML = "";
 
 	// Get all segments
-	segments = $('.gws-flights-results__slice-selection');
+	var segments = $('.gws-flights-results__slice-selection');
 
 	// Iterate over each segment
 	segments.each(function(index){
-		originDate = new Date($(this).find('.gws-flights-results__itinerary-times>.gws-flights-results__times-row>span.flt-subhead1').text());
-		legs = $(this).find('.gws-flights-results__leg');
+		var originDate = new Date($(this).find('.gws-flights-results__itinerary-times>.gws-flights-results__times-row>span.flt-subhead1').text());
+		var legs = $(this).find('.gws-flights-results__leg');
 		legs.each(function(index){
-			flightNumber = $(this).find('.gws-flights-results__other-leg-info>span:last').text().replace(/\s/g, '');
-			airline = $(this).find('.gws-flights-results__leg-flight>div:first').text();
-			travelTime = $(this).find('.gws-flights-results__leg-duration>div:first>span').text().replace(/\s/g, '');
-			departureAirport = $(this).find('.gws-flights-results__leg-departure>div:last>span:first').text();
-			departureIATACode = $(this).find('.gws-flights-results__leg-departure').find('.gws-flights-results__iata-code').text();
-			departureOffSetDate = $(this).find('.gws-flights-results__leg-departure').find('.gws-flights__offset-days').text();
+			var flightNumber = $(this).find('.gws-flights-results__other-leg-info>span:last').text().replace(/\s/g, '');
+			var airline = $(this).find('.gws-flights-results__leg-flight>div:first').text();
+			var travelTime = $(this).find('.gws-flights-results__leg-duration>div:first>span').text().replace(/\s/g, '');
+			var departureAirport = $(this).find('.gws-flights-results__leg-departure>div:last>span:first').text();
+			var departureIATACode = $(this).find('.gws-flights-results__leg-departure').find('.gws-flights-results__iata-code').text();
+			var departureOffSetDate = $(this).find('.gws-flights-results__leg-departure').find('.gws-flights__offset-days').text();
 			if(departureOffSetDate == '') {
 				departureOffSetDate = 0;
 			}
-			departureDate = new Date(originDate);
+			var departureDate = new Date(originDate);
 			departureDate.setDate(originDate.getDate()+parseInt(departureOffSetDate));
-			departureTime = $(this).find('.gws-flights-results__leg-departure>div:first>span>span:first').text();
+			var departureTime = $(this).find('.gws-flights-results__leg-departure>div:first>span>span:first').text();
 
-			arrivalAirport = $(this).find('.gws-flights-results__leg-arrival>div:last>span:first').text();
-			arrivalIATACode = $(this).find('.gws-flights-results__leg-arrival').find('.gws-flights-results__iata-code').text();
-			arrivalOffSetDate = $(this).find('.gws-flights-results__leg-arrival').find('.gws-flights__offset-days').text();
+			var arrivalAirport = $(this).find('.gws-flights-results__leg-arrival>div:last>span:first').text();
+			var arrivalIATACode = $(this).find('.gws-flights-results__leg-arrival').find('.gws-flights-results__iata-code').text();
+			var arrivalOffSetDate = $(this).find('.gws-flights-results__leg-arrival').find('.gws-flights__offset-days').text();
 			if(arrivalOffSetDate == '') {
 				arrivalOffSetDate = 0;
 			}
-			arrivalDate = new Date(originDate);
+			var arrivalDate = new Date(originDate);
 			arrivalDate.setDate(originDate.getDate()+parseInt(arrivalOffSetDate));
-			arrivalTime = $(this).find('.gws-flights-results__leg-arrival>div:first>span>span:first').text();
-			leg = "";
+			var arrivalTime = $(this).find('.gws-flights-results__leg-arrival>div:first>span>span:first').text();
+			var leg = "";
 			leg = leg + "*"+airline+ " "+flightNumber+"* (dur: "+travelTime+")" +"\r\n";
 			leg = leg + "Departure "+departureAirport + " ("+departureIATACode+") "+departureDate.toLocaleDateString('en-GB', {'day':'2-digit', 'month':'short'})+" "+ departureTime+"\r\n";
 			leg = leg + "Arrival "+arrivalAirport + " ("+arrivalIATACode+") "+arrivalDate.toLocaleDateString('en-GB', {'day':'2-digit', 'month':'short'}) + " " +arrivalTime+"\r\n";
 
-			legHTML = "";
+			var legHTML = "";
 			legHTML = legHTML + "<b>"+airline+ " "+flightNumber+"</b> (dur: "+travelTime+")" +"<br/>";
 			legHTML = legHTML + "Departure "+departureAirport + " ("+departureIATACode+") "+departureDate.toLocaleDateString('en-GB', {'day':'2-digit', 'month':'short'})+" "+ departureTime+"<br/>";
 			legHTML = legHTML + "Arrival "+arrivalAirport + " ("+arrivalIATACode+") "+arrivalDate.toLocaleDateString('en-GB', {'day':'2-digit', 'month':'short'}) + " " +arrivalTime+"<br/>";
